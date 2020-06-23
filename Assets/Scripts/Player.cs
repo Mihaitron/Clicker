ccc34c956212f03e970569f025557772a6c2dce8
Mihaitron <popamihai18@yahooc.om>
2020-06-23 19:00:14 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int coinsPerClick;
+
+    private int totalCoins = 0;
+    private Text coinsAmount;
+
+    private void Start()
+    {
+        this.coinsAmount = GameObject.Find("Coins Amount").GetComponent<Text>();
+    }
+
+    private void OnMouseDown()
+    {
+        this.AddCoins(this.coinsPerClick);
+    }
+
+    public void AddCoins(int coins)
+    {
+        this.totalCoins += coins;
+        this.coinsAmount.text = this.totalCoins.ToString();
+    }
+}
