9cda16d28d426bfef1d84923799403693d6f0f3c
Popa Mihai <popamihai18@yahoo.com>
2020-07-09 18:04:56 +0300

    New item to buy and new prices/production
    Added a new item to buy: Fast Clicker - base cost 100 & production 1
    Changed Clicker - base cost 15 & production 0.1
    Changed formula to base*1.15^N, where N is the number of items

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,25 +9,26 @@ public class Player : MonoBehaviour
     [Header("Player")]
     public float coinsPerClick;
 
-    private float totalCoins = 0f;
+    private float totalCoins = 0;
     private Text coinsAmount;
 
     public void AddCoins(float coins)
     {
         this.totalCoins += coins;
-        this.coinsAmount.text = this.totalCoins.ToString();
+        this.coinsAmount.text = this.totalCoins.ToString("F1");
     }
 
     public void SubtractCoins(float coins)
     {
         this.totalCoins -= coins;
-        this.coinsAmount.text = this.totalCoins.ToString();
+        this.coinsAmount.text = this.totalCoins.ToString("F1");
     }
 
     private IEnumerator AddCoinsPerSecond()
     {
         float clicker_coins = this.numberOfClickers * this.clickerProduction;
-        float total_coins_to_add = clicker_coins;
+        float fast_clicker_coins = this.numberOfFastClickers * this.fastClickerProduction;
+        float total_coins_to_add = clicker_coins + fast_clicker_coins;
 
         this.AddCoins(total_coins_to_add);
 
@@ -39,25 +40,51 @@ public class Player : MonoBehaviour
 
     #region Clicker
     [Header("Clicker")]
-    public float clickerProduction = 0.5f;
+    public float clickerProduction = 0.1f;
 
-    private float clickerBaseCost = 10;
+    private float clickerBaseCost = 15;
     private int numberOfClickers = 0;
 
     public void BuyClicker()
     {
-        float clicker_cost = this.clickerBaseCost * (this.numberOfClickers + 1) + this.numberOfClickers;
+        int clicker_cost = this.calculateCost(this.clickerBaseCost, this.numberOfClickers);
 
         if (this.totalCoins >= clicker_cost)
         {
             this.SubtractCoins(clicker_cost);
             this.numberOfClickers++;
 
-            Debug.Log("You now have " + this.numberOfClickers + " clickers");
+            Debug.Log("You now have " + this.numberOfClickers + " clickers for " + clicker_cost);
 
-            float new_cost = this.clickerBaseCost * (this.numberOfClickers + 1) + this.numberOfClickers;
+            float new_cost = this.calculateCost(this.clickerBaseCost, this.numberOfClickers);
 
             GameObject.Find("BuyClickers").transform.GetChild(1).gameObject.GetComponent<Text>().text = new_cost.ToString() + " coins";
+        }
+    }
+    #endregion
+
+    #region Fast Clicker
+    [Header("FastClicker")]
+    public float fastClickerProduction = 1f;
+
+
+    private float fastClickerBaseCost = 100;
+    private int numberOfFastClickers = 0;
+
+    public void BuyFastClicker()
+    {
+        int fast_clicker_cost = this.calculateCost(this.fastClickerBaseCost, this.numberOfFastClickers);
+
+        if (this.totalCoins >= fast_clicker_cost)
+        {
+            this.SubtractCoins(fast_clicker_cost);
+            this.numberOfFastClickers++;
+
+            Debug.Log("You now have " + this.numberOfFastClickers + " fast clickers for " + fast_clicker_cost);
+
+            float new_cost = this.calculateCost(this.fastClickerBaseCost, this.numberOfFastClickers);
+
+            GameObject.Find("BuyFastClickers").transform.GetChild(1).gameObject.GetComponent<Text>().text = new_cost.ToString() + " coins";
         }
     }
     #endregion
@@ -66,11 +93,19 @@ public class Player : MonoBehaviour
     {
         this.coinsAmount = GameObject.Find("Coins Amount").GetComponent<Text>();
 
+        GameObject.Find("BuyClickers").transform.GetChild(1).gameObject.GetComponent<Text>().text = this.clickerBaseCost.ToString() + " coins";
+        GameObject.Find("BuyFastClickers").transform.GetChild(1).gameObject.GetComponent<Text>().text = this.fastClickerBaseCost.ToString() + " coins";
+
         StartCoroutine(this.AddCoinsPerSecond());
     }
 
     private void OnMouseDown()
     {
         this.AddCoins(this.coinsPerClick);
+    }
+
+    private int calculateCost(float base_cost, float number)
+    {
+        return (int)(base_cost * Mathf.Pow(1.15f, number));
     }
 }
